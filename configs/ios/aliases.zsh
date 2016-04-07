b9ad22013560313a634c8c1b72ea26ca3d43f36a
Bruno de Carvalho <bruno@biasedbit.com>
2016-04-07 19:32:32 -0400

    Add codesign_ids and nuke_derived_data aliases

--- configs/ios/aliases.zsh
+++ configs/ios/aliases.zsh
@@ -1,1 +1,3 @@
 alias simulator="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+alias nuke_derived_data="rm -rf ~/Library/Developer/Xcode/DerivedData/*"
+alias codesign_ids="security find-identity -v -p codesigning"
