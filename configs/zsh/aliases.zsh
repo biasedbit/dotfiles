92f8fda35282db74f999ab7dff69b03a98d198b8
Bruno de Carvalho <bruno@biasedbit.com>
2018-08-06 22:34:01 -0700

    Add git grep alias

--- configs/zsh/aliases.zsh
+++ configs/zsh/aliases.zsh
@@ -8,3 +8,5 @@ alias ls="ls -Gh"
 
 # Pipe public key to clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+alias gg="git grep --break --heading --line-number"
