d75b8337ac92e5410aeaf2730ed3a8d04cb659d9
Bruno de Carvalho <bruno@biasedbit.com>
2018-08-10 21:47:35 -0700

    Add shortcut to copy current git branch name to clipboard

--- configs/zsh/aliases.zsh
+++ configs/zsh/aliases.zsh
@@ -9,4 +9,8 @@ alias ls="ls -Gh"
 # Pipe public key to clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
+# gitgrep w/ breaks
 alias gg="git grep --break --heading --line-number"
+
+# copy current git branch name to clipboard
+alias gbcp="git rev-parse --abbrev-ref HEAD | tr -d '\n' | pbcopy"
