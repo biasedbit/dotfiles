041d89d3bd403bdf6850707825401dc0c0223c52
Bruno de Carvalho <bruno@biasedbit.com>
2022-10-18 21:48:56 -0700

    Fix clipboard-dependent aliases for Linux

--- configs/zsh/aliases.zsh
+++ configs/zsh/aliases.zsh
@@ -7,16 +7,18 @@ alias reload!='. ~/.zshrc'
 case "$OSTYPE" in
   darwin*)
     alias ls="ls -Gh"
+    CLIPBOARD_CMD="pbcopy"
     ;;
   *)
     alias ls="ls -Gh --color --group-directories-first -X"
+    CLIPBOARD_CMD="xsel -ib"
 esac
 
 # Pipe public key to clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_rsa.pub | $CLIPBOARD_CMD | echo '=> Public key copied to pasteboard.'"
 
 # gitgrep w/ breaks
 alias gg="git grep --break --heading --line-number"
 
 # copy current git branch name to clipboard
-alias gbcp="git rev-parse --abbrev-ref HEAD | tr -d '\n' | pbcopy"
+alias gbcp="git rev-parse --abbrev-ref HEAD | tr -d '\n' | $CLIPBOARD_CMD"
