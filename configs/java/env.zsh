b2344a9ab6c52c9bb554b4d2e85f20f0daad1591
Bruno de Carvalho <bruno@biasedbit.com>
2022-10-18 22:46:35 -0700

    Update macos java version to 19

--- configs/java/env.zsh
+++ configs/java/env.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 case "$OSTYPE" in
   darwin*)
-    export JAVA_HOME=$(/usr/libexec/java_home -Fv 18)
+    export JAVA_HOME=$(/usr/libexec/java_home -Fv 19)
     ;;
   linux-gnu*)
     export JAVA_HOME=$(update-alternatives --query java | grep 'Value: ' | sed -e 's/Value: //;s?/bin/java?/?;')
