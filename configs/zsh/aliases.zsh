a1d8c78fc57d858cde4e21757379239c88574f37
Bruno de Carvalho <bruno@biasedbit.com>
2022-12-21 02:44:23 -0800

    WSL clipboard support + cosmetics

--- configs/zsh/aliases.zsh
+++ configs/zsh/aliases.zsh
@@ -8,20 +8,28 @@ case "$OSTYPE" in
   darwin*)
     alias ls="ls -Gh"
     alias update="brew update && brew upgrade"
-    CLIPBOARD_CMD="pbcopy"
+    clipboard_cmd="pbcopy"
     ;;
-  *)
+  "linux-gnu")
     alias ls="ls -Gh --color --group-directories-first -X"
     alias update="sudo apt update && sudo apt upgrade"
-    CLIPBOARD_CMD="xsel -ib"
+    if [[ $(grep microsoft /proc/version) ]]; then
+      # Full path since wsl.conf will normally has appendWindowsPath = false
+      clipboard_cmd="/mnt/c/Windows/System32/clip.exe"
+    else
+      clipboard_cmd="xsel -ib"
+    fi
+    ;;
+  *)
 esac
 
+alias cbcopy=$clipboard_cmd
+
 # Pipe public key to clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | $CLIPBOARD_CMD | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_rsa.pub | cbcopy | echo '=> Public key copied to pasteboard.'"
 
 # gitgrep w/ breaks
 alias gg="git grep --break --heading --line-number"
 
 # copy current git branch name to clipboard
-alias gbcp="git rev-parse --abbrev-ref HEAD | tr -d '\n' | $CLIPBOARD_CMD"
-
+alias gbcp="git rev-parse --abbrev-ref HEAD | tr -d '\n' | cbcopy"
