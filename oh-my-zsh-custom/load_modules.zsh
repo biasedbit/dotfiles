1b7ef7bc924eb4155acbffaac5ba7512108116b3
Bruno de Carvalho <bruno@biasedbit.com>
2022-12-21 02:50:01 -0800

    Simplify env loading step
    Source all config/*/*.zsh files. These should return early when not
    running in the right env.
    
    With this approach, ordering support is lost.

--- oh-my-zsh-custom/load_modules.zsh
+++ oh-my-zsh-custom/load_modules.zsh
@@ -3,22 +3,11 @@
 CONFIG_DIR="$(cd "$(dirname "$0")/.." && pwd -P)/configs"
 
 load_configs() {
-  local modules= # list of modules to source *.zsh files from; order matters
-  case "$OSTYPE" in
-    darwin*)
-      modules=('git' 'homebrew' 'java' 'ruby' 'zsh')
-      ;;
-    "linux-gnu")
-      modules=('git' 'java' 'ruby' 'zsh')
-      ;;
-    *)
-      echo "! Unsupported OS ($OSTYPE); skipping custom module load."
-      ;;
-  esac
-
-  for module in ${modules}; do
-    local files=($CONFIG_DIR/$module/*.zsh)
-    for file in $files; do source $file; done
+  # Source all config/*/*.zsh files; these are expected to no-op when not\
+  # running in their expected env (if any).
+  local files=($CONFIG_DIR/*/*.zsh)
+  for file in $files; do
+    source $file
   done
 }
 
