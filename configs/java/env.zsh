f6c128139ec2cc82f56f7c5a2d7ffb713609d75c
Bruno de Carvalho <bruno@biasedbit.com>
2022-12-23 01:33:48 -0800

    Use jenv for global/project java version management

--- configs/java/env.zsh
+++ configs/java/env.zsh
@@ -1,9 +1,36 @@
 #!/usr/bin/env zsh
-case "$OSTYPE" in
-  darwin*)
-    export JAVA_HOME=$(/usr/libexec/java_home -Fv 19)
-    ;;
-  linux-gnu*)
-    export JAVA_HOME=$(update-alternatives --query java | grep 'Value: ' | sed -e 's/Value: //;s?/bin/java?/?;')
-    ;;
-esac
+
+# `jenv init -`, minus completions
+export PATH="$HOME/.jenv/shims:${PATH}"
+export JENV_SHELL=zsh
+export JENV_LOADED=1
+unset JAVA_HOME
+unset JDK_HOME
+# source '(...)/completions/jenv.zsh'
+jenv rehash 2>/dev/null
+jenv refresh-plugins
+jenv() {
+  type typeset &> /dev/null && typeset command
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  enable-plugin|rehash|shell|shell-options)
+    eval `jenv "sh-$command" "$@"`;;
+  *)
+    command jenv "$command" "$@";;
+  esac
+}
+
+jenv global 19.0
+
+# case "$OSTYPE" in
+#   darwin*)
+#     export JAVA_HOME=$(/usr/libexec/java_home -Fv 19)
+#     ;;
+#   linux-gnu*)
+#     export JAVA_HOME=$(update-alternatives --query java | grep 'Value: ' | sed -e 's/Value: //;s?/bin/java?/?;')
+#     ;;
+# esac
