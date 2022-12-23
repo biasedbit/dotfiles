c3aacc6fb9789179825684985dbbab3b620d5a76
Bruno de Carvalho <bruno@biasedbit.com>
2022-12-23 01:34:37 -0800

    Update aliases

--- configs/zsh/aliases.zsh
+++ configs/zsh/aliases.zsh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
 # Reload shell
-alias reload!='. ~/.zshrc'
+alias reload!='exec zsh'
+# SafetyThird™
+alias rm='rm -I'
+# Disk usage of items in current dir
+alias dud='du -d 1 -h'
 
-# Print all ls * with human readable sizes and colors
 case "$OSTYPE" in
   darwin*)
     alias ls="ls -Gh"
@@ -14,7 +17,7 @@ case "$OSTYPE" in
     alias ls="ls -Gh --color --group-directories-first -X"
     alias update="sudo apt update && sudo apt upgrade"
     if [[ $(grep microsoft /proc/version) ]]; then
-      # Full path since wsl.conf will normally has appendWindowsPath = false
+      # Full path since wsl.conf will normally have appendWindowsPath = false
       clipboard_cmd="/mnt/c/Windows/System32/clip.exe"
     else
       clipboard_cmd="xsel -ib"
@@ -25,11 +28,9 @@ esac
 
 alias cbcopy=$clipboard_cmd
 
-# Pipe public key to clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | cbcopy | echo '=> Public key copied to pasteboard.'"
-
 # gitgrep w/ breaks
 alias gg="git grep --break --heading --line-number"
 
 # copy current git branch name to clipboard
 alias gbcp="git rev-parse --abbrev-ref HEAD | tr -d '\n' | cbcopy"
+alias gpom="git pull origin main"
