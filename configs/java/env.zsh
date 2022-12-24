2626f49d0bcf1360ee16449855b1fa2666df3190
Bruno de Carvalho <bruno@biasedbit.com>
2022-12-24 03:08:46 -0800

    Use jenv plugin

--- configs/java/env.zsh
+++ configs/java/env.zsh
@@ -5,29 +5,7 @@ if [[ "$OSTYPE" != "gnu-linux" ]]; then
   export PATH="$HOME/.jenv/bin:$PATH"
 fi
 
-# `jenv init -`, minus completions
-export PATH="$HOME/.jenv/shims:${PATH}"
-export JENV_SHELL=zsh
-export JENV_LOADED=1
-unset JAVA_HOME
-unset JDK_HOME
-# source '(...)/completions/jenv.zsh'
-jenv rehash 2>/dev/null
-jenv refresh-plugins
-jenv() {
-  type typeset &> /dev/null && typeset command
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  enable-plugin|rehash|shell|shell-options)
-    eval `jenv "sh-$command" "$@"`;;
-  *)
-    command jenv "$command" "$@";;
-  esac
-}
+jenv global 19.0
 
 # case "$OSTYPE" in
 #   darwin*)
