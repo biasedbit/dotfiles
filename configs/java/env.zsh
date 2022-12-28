6b48b1853c55eb115bac262f60657ea906f3f86a
Bruno de Carvalho <bruno@biasedbit.com>
2022-12-27 23:34:11 -0800

    Fix jenv path setting in Linux + cosmetics

--- configs/java/env.zsh
+++ configs/java/env.zsh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
-# In linux, jenv is installed by cloning repo to ~/.jenv
-if [[ "$OSTYPE" != "gnu-linux" ]]; then
+# On macOS, jenv is installed via Homebrew, and its binary is available at
+# /opt/homebrew/bin (added to path by brew omz plugin). In linux, this binary
+# must be added to path manually, since jenv is installed via git clone.
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
   export PATH="$HOME/.jenv/bin:$PATH"
 fi
 
--- configs/java/install.sh
+++ configs/java/install.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-# Managed with homebrew in macos
+# On macOS, jenv is installed via homebrew; nothing to do here.
 if [[ "$OSTYPE" != "linux-gnu" ]]; then exit; fi
 
 if [ ! -d "$HOME/.jenv" ] ; then
   git clone --depth 1 https://github.com/jenv/jenv.git $HOME/.jenv
+else
+  cd $HOME/.jenv
+  git pull origin master
 fi
 
 mkdir -p $HOME/.jenv/versions
+# Add JDK installed in Aptfile (openjdk-19-jdk) to managed java versions
 $HOME/.jenv/bin/jenv add /usr/lib/jvm/java-19-openjdk-amd64
