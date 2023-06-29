0ae4d433c1987b3daad92bab0418ccf7a2c53929
Bruno de Carvalho <bruno@biasedbit.com>
2023-06-29 15:08:34 -0700

    Add command to nuke homebrew

--- configs/homebrew/env.zsh
+++ configs/homebrew/env.zsh
@@ -2,3 +2,12 @@
 if [[ "$OSTYPE" != darwin* ]]; then return; fi
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+function nuke_homebrew() {
+  # Switch back to bash to avoid new sessions breaking due to
+  # missing zsh (deleted in the purge process below)
+  chsh -s /bin/bash
+  rm -rf ~/Library/Caches/Homebrew
+  /opt/homebrew/bin/brew remove --cask --force $(/opt/homebrew/bin/brew list)
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+}
