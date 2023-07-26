677ef45709c311192afa721e9dac2827533da5b9
Bruno de Carvalho <bruno@biasedbit.com>
2023-07-25 19:14:45 -0700

    Correct jdk path in ubuntu

--- configs/java/install.sh
+++ configs/java/install.sh
@@ -15,4 +15,4 @@ fi
 
 mkdir -p $HOME/.jenv/versions
 # Add JDK installed in Aptfile (openjdk-20-jdk) to managed java versions
-$HOME/.jenv/bin/jenv add /usr/lib/jvm/java-20-openjdk-amd64/Contents/Home
+$HOME/.jenv/bin/jenv add /usr/lib/jvm/java-20-openjdk-amd64/
