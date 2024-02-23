be17d0f8925db003fd53b0ea3c1253118ec79889
Bruno de Carvalho <bruno@biasedbit.com>
2024-02-22 22:49:30 -0800

    Use recommended asdf install/setup

--- configs/asdf/install.sh
+++ configs/asdf/install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh
 # set -e
 
+rm -rf ~/.asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+. "$HOME/.asdf/asdf.sh"
+
 asdf plugin add nodejs || true
 asdf plugin add python || true
 asdf plugin add ruby || true
