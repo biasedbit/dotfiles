c5b4431fbd5bf36d6eb146b8eeafa2029f7420f1
Bruno de Carvalho <bruno@biasedbit.com>
2024-02-23 07:18:56 -0800

    Ruby & python build adjustments

--- configs/asdf/install.sh
+++ configs/asdf/install.sh
@@ -6,8 +6,9 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 . "$HOME/.asdf/asdf.sh"
 
 asdf plugin add nodejs || true
-asdf plugin add python || true
-asdf plugin add ruby || true
 asdf plugin add pnpm || true
+asdf plugin add python || true
+asdf plugin add poetry || true
+asdf plugin add ruby || true
 asdf plugin add java || true
 asdf install
