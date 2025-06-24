6a5ce6299d595d692988fedb66010be043193934
Bruno de Carvalho <bruno@biasedbit.com>
2025-06-23 20:25:26 -0700

    feat: nvim config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- Show line numbers
+vim.opt.number = true
+
+-- Render invisibles
+vim.opt.listchars = {
+  tab = '→ ',
+  space = '·',
+  nbsp = '␣',
+  trail = '•',
+  eol = '¬',
+  precedes = '«',
+  extends = '»'
+}
+
